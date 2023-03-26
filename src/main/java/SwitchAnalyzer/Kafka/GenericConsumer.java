2c1e9e1ad875051509c228a85359851007891524
ZiadFahmy <68162606+zyadfahmy2@users.noreply.github.com>
2023-03-26 03:07:06 +0200

    added byeArray to the consumer

--- src/main/java/SwitchAnalyzer/Kafka/GenericConsumer.java
+++ src/main/java/SwitchAnalyzer/Kafka/GenericConsumer.java
@@ -3,13 +3,16 @@ package SwitchAnalyzer.Kafka;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 
 import java.time.Duration;
 import java.util.Arrays;
 import java.util.Properties;
 
 public class GenericConsumer {
-    private final KafkaConsumer<String, String> consumer;
+    private  KafkaConsumer<String, String> consumer;
+    private KafkaConsumer<String, byte[]> byteArrayConsumer;
     private  String topic;
 
     public GenericConsumer(String IP,String groupId) {
@@ -21,6 +24,19 @@ public class GenericConsumer {
         //TODO: note this must be removed it is just for testing
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         this.consumer = new KafkaConsumer<>(props);
+    }
+    public GenericConsumer(String IP,String groupId,boolean flag) {
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,IP);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG,"latest");
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, groupId);
+        this.byteArrayConsumer = new KafkaConsumer<String,byte[]>(properties);
+    }
+    public void  selectTopicByteArray(String topic)
+    {
+        this.byteArrayConsumer.subscribe(Arrays.asList(topic));
 
     }
     public void  selectTopic(String topic)
@@ -29,16 +45,15 @@ public class GenericConsumer {
 
     }
     public ConsumerRecords<String, String> consume(int milliSeconds) {
-
-
-//        while (true) {
             ConsumerRecords<String, String> records = this.consumer.poll(Duration.ofMillis(milliSeconds));
-//            for (ConsumerRecord<String, String> record : records) {
-//                System.out.printf("offset = %d, key = %s, value = %s\n", record.offset(), record.key(), record.value());
-//            }
-//        }
+
         return records;
     }
+    public ConsumerRecords<String, byte[]> consumeByteArray(int milliSeconds) {
+        ConsumerRecords<String, byte[]> records = this.byteArrayConsumer.poll(Duration.ofMillis(milliSeconds));
+        return records;
+    }
+
 
     public void close() {
         this.consumer.close();
