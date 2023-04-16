92a0ba3cec964490f85e6d03f15e8d960e416940
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-04-16 21:51:46 +0200

    Frame retrieval for user

--- src/main/java/SwitchAnalyzer/Collectors/MOMConsumer.java
+++ src/main/java/SwitchAnalyzer/Collectors/MOMConsumer.java
@@ -24,7 +24,7 @@ import static SwitchAnalyzer.MainHandler_MOM.masterOfMasters;
  */
 
 public class MOMConsumer {
-    static String consumerGroup = "MOMCollector";
+    static String consumerGroup = "MOMColBHJJlector";
     static GenericConsumer consumer = new GenericConsumer(IP.ip1 + ":" + Ports.port1, consumerGroup);
     //arraylist of collectors
     public static ArrayList<Collector> collectors = new ArrayList<>();
--- src/main/java/SwitchAnalyzer/Kafka/Producer.java
+++ src/main/java/SwitchAnalyzer/Kafka/Producer.java
@@ -9,6 +9,7 @@ public class Producer
     public Producer(String ip) {  producer = new GenericProducer(ip + ":" + Ports.port1); }
     public void produce(String o, String topic)
     {
+        System.out.println(o);
         producer.send(topic,o);
     }
     public void flush() { producer.flush(); }
