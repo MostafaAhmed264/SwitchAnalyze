83840f66d7cfff0ac5ad9efab9738601afec8e28
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-04-22 23:37:47 +0200

    Update FrameProcessing.java

--- src/main/java/SwitchAnalyzer/Network/FrameProcessing.java
+++ src/main/java/SwitchAnalyzer/Network/FrameProcessing.java
@@ -1,9 +1,15 @@
 package SwitchAnalyzer.Network;
 
+import SwitchAnalyzer.Kafka.Topics;
+import SwitchAnalyzer.MainHandler_Node;
+import org.pcap4j.core.PacketListener;
+import org.pcap4j.core.PcapHandle;
 import org.pcap4j.core.PcapPacket;
 import org.pcap4j.packet.EthernetPacket;
+import org.pcap4j.packet.IpV4Packet;
 import org.pcap4j.packet.Packet;
 import org.pcap4j.packet.UnknownPacket;
+import org.pcap4j.util.MacAddress;
 
 import java.util.Arrays;
 
@@ -14,13 +20,49 @@ public class FrameProcessing
         FrameResult frameResult = new FrameResult();
         Packet.Builder payloadBuild = Builder.getBuilder(new PayloadBuilder(new String(frameBytes)),
                 new UnknownPacket.Builder());
-        Packet packet = payloadBuild.build();
-        System.out.println(packet);
+        EthernetPacket packet = null;
+        try { packet = EthernetPacket.newPacket(frameBytes,0,frameBytes.length); }
+        catch (Exception e) { e.printStackTrace(); }
+        Packet.Builder builder =  packet.getBuilder();
+        Packet.Builder builder1 =  builder.getPayloadBuilder();
+        Packet.Builder builder2 = builder1.getPayloadBuilder();
+        Packet.Builder builder3 = builder2.getPayloadBuilder();
+        System.out.println(getType(builder.build().getHeader()));
+        System.out.println(getType(builder1.build().getHeader()));
+        System.out.println(getType(builder2.build().getHeader()));
+
+
+
         return frameResult;
     }
 
+    public static String getType(Packet.Header header)
+    {
+        String headerString = header.toString();
+        StringBuilder result = new StringBuilder("");
+        int i = 0;
+        while(headerString.charAt(i) != ']')
+        {
+            char c = headerString.charAt(i++);
+            if (c == '[')
+                continue;
+            if (c == '(')
+                break;
+            result.append(c);
+        }
+        return result.toString();
+    }
     public static void main(String[] args)
     {
-
+        PCAP.initialize();
+        PcapHandle handle = PCAP.createHandle();
+        PacketListener listener =
+                pcapPacket ->
+                {
+                    System.out.println(pcapPacket);
+                    processFrames(pcapPacket.getRawData());
+                };
+        try { handle.loop(1,listener); }
+        catch (Exception e) { e.printStackTrace(); }
     }
 }
