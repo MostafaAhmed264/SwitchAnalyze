4ec000a29630e4b30015ddc24b5c9b42a78c269a
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-04-24 01:27:19 +0200

    Update SystemMaps.java

--- src/main/java/SwitchAnalyzer/miscellaneous/SystemMaps.java
+++ src/main/java/SwitchAnalyzer/miscellaneous/SystemMaps.java
@@ -48,9 +48,9 @@ public class SystemMaps
         GlobalVariable.portHpcMap.put(2, master2);
 
         master1.childNodes.add(new MachineNode(0));
-        master1.childNodes.add(new MachineNode(1));
+        //master1.childNodes.add(new MachineNode(1));
         master2.childNodes.add(new MachineNode(0));
-        master2.childNodes.add(new MachineNode(1));
+        //master2.childNodes.add(new MachineNode(1));
 
         MainHandler_MOM.masterOfMasters = new MOM();
         MainHandler_MOM.masterOfMasters.HPCs.add(master1);
@@ -90,14 +90,14 @@ public class SystemMaps
 
         try
         {
-            master1.HPCMacAddr = Builder.buildMacAddress("38:22:E2:F1:C8:E7");
-            master1.HPCIp = Builder.buildIpV4Address("192.168.1.35");
+            master1.HPCMacAddr = Builder.buildMacAddress("54:EE:75:DF:82:C4");
+            master1.HPCIp = Builder.buildIpV4Address("192.168.1.100");
 
-            master2.HPCMacAddr = Builder.buildMacAddress("38:22:E2:F1:C8:E7");
-            master2.HPCIp = Builder.buildIpV4Address("192.168.1.35");
+            master2.HPCMacAddr = Builder.buildMacAddress("54:EE:75:DF:82:C4");
+            master2.HPCIp = Builder.buildIpV4Address("192.168.1.100");
 
-            machine1.nodeMacAddress = Builder.buildMacAddress("34:E6:D7:5E:01:CE");
-            machine1.nodeIp = Builder.buildIpV4Address("192.168.1.60");
+            machine1.nodeMacAddress = Builder.buildMacAddress("8C:16:45:21:C5:E7");
+            machine1.nodeIp = Builder.buildIpV4Address("192.168.1.35");
 
             //machine2.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
             //  machine2.nodeIp = Builder.buildIpV4Address("192.168.1.100");
@@ -130,37 +130,37 @@ public class SystemMaps
         MasterOfHPC master1 = new MasterOfHPC(0);
         MasterOfHPC master2 = new MasterOfHPC(1);
         MachineNode machine1 = new MachineNode(0);
-        MachineNode machine2 = new MachineNode(1);
+        //MachineNode machine2 = new MachineNode(1);
         MachineNode machine3 = new MachineNode(0);
-        MachineNode machine4 = new MachineNode(1);
+        //MachineNode machine4 = new MachineNode(1);
 
         GlobalVariable.portHpcMap.put(1, master1);
         GlobalVariable.portHpcMap.put(2, master2);
 
         master1.childNodes.add(machine1);
-        master1.childNodes.add(machine2);
+       // master1.childNodes.add(machine2);
         master2.childNodes.add(machine3);
-        master2.childNodes.add(machine4);
+       // master2.childNodes.add(machine4);
 
         try
         {
-            master1.HPCMacAddr = Builder.buildMacAddress("00:00:00:00:00:01");
+            master1.HPCMacAddr = Builder.buildMacAddress("54:EE:75:DF:82:C4");
             master1.HPCIp = Builder.buildIpV4Address("192.168.1.100");
 
-            master2.HPCMacAddr = Builder.buildMacAddress("00:00:00:00:00:01");
+            master2.HPCMacAddr = Builder.buildMacAddress("54:EE:75:DF:82:C4");
             master2.HPCIp = Builder.buildIpV4Address("192.168.1.100");
 
-            machine1.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
-            machine1.nodeIp = Builder.buildIpV4Address("192.168.1.100");
+            machine1.nodeMacAddress = Builder.buildMacAddress("8C:16:45:21:C5:E7");
+            machine1.nodeIp = Builder.buildIpV4Address("192.168.1.35");
 
-            machine2.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
-            machine2.nodeIp = Builder.buildIpV4Address("192.168.1.100");
+//            machine2.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
+//            machine2.nodeIp = Builder.buildIpV4Address("192.168.1.100");
 
-            machine3.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
-            machine3.nodeIp = Builder.buildIpV4Address("192.168.1.100");
+            machine3.nodeMacAddress = Builder.buildMacAddress("3C:2C:30:9B:3B:90");
+            machine3.nodeIp = Builder.buildIpV4Address("192.168.1.101");
 
-            machine4.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
-            machine4.nodeIp = Builder.buildIpV4Address("192.168.1.100");
+//            machine4.nodeMacAddress = Builder.buildMacAddress("00:00:00:00:00:01");
+//            machine4.nodeIp = Builder.buildIpV4Address("192.168.1.100");
         }
         catch (Exception ignored){}
 
