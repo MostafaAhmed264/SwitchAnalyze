3c3d4f77448f27729d3a18bdae11a2562ca5292c
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-04-27 03:30:17 +0200

    Create CountTypeCollectorMOM.java
    *CountTypeCollectorMOM -> Adds a module to count the total number of packets in the system according to the type.

---
+++ src/main/java/SwitchAnalyzer/Collectors/CountTypeCollectorMOM.java
@@ -0,0 +1,49 @@
+package SwitchAnalyzer.Collectors;
+
+import SwitchAnalyzer.Machines.MOM;
+import SwitchAnalyzer.NamingConventions;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import static SwitchAnalyzer.MainHandler_MOM.masterOfMasters;
+
+public class CountTypeCollectorMOM implements Collector
+{
+    public String collect()
+    {
+        ArrayList<String> subsetKeys = new ArrayList<>();
+        long totalCount = 0;
+        for (int i = 0; i < masterOfMasters.HPCs.size(); i++)
+        {
+            subsetKeys = getKeys(masterOfMasters.HPCs.get(i).hpcInfo.map.keySet());
+            for (String key: subsetKeys)
+            {
+                long currTypeNoPacketsFromSingleHPC = Long.parseLong(masterOfMasters.HPCs.get(i).hpcInfo.map.get(key));
+                MOMConsumer.results.putIfAbsent(key.substring(3), "0");
+                long currResultNoPackets = Long.parseLong(MOMConsumer.results.get(key.substring(3)));
+                MOMConsumer.results.put(key.substring(3),String.valueOf(currResultNoPackets  + currTypeNoPacketsFromSingleHPC));
+            }
+        }
+        return "-1";
+    }
+    public String getName()
+    {
+        return NamingConventions.countTypeAmount;
+    }
+
+    public ArrayList<String> getKeys (Set<String> keys)
+    {
+        ArrayList<String> result = new ArrayList<>();
+        for (String key : keys)
+        {
+            if(key.startsWith("XXX"))
+            {
+               result.add(key);
+            }
+        }
+        return result;
+    }
+
+
+}
