1a44dcee69352e8e9d61dd7d03539504faeaf1e3
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-04-27 04:30:50 +0200

    Update SystemInitializer.java

--- src/main/java/SwitchAnalyzer/SystemInitializer/SystemInitializer.java
+++ src/main/java/SwitchAnalyzer/SystemInitializer/SystemInitializer.java
@@ -17,20 +17,19 @@ public class SystemInitializer {
         MOM masterOfMasters = new MOM(myNode);
         masterOfMasters.setHPCsInformation(momConfigurations.cluster);
         MainHandler_MOM.masterOfMasters = masterOfMasters;
-
     }
 
-    public static void MasterInit(MachineNode myNode, MachineConfigurations machineConfig, ClusterConfiguartions clusterConfig) {
+    public static void MasterInit(MachineNode myNode, MachineConfigurations machineConfig, ClusterConfiguartions clusterConfig)
+    {
         myNode.setMachineID(machineConfig.getMachine_id());
         myNode.setNodeIp(machineConfig.getIp());
         MasterOfHPC master = new MasterOfHPC(clusterConfig.getCluster_Id(), clusterConfig.getCluster_name(), myNode);
         master.setChildNodes(clusterConfig.machines);
-
         MainHandler_Master.master = master;
-
     }
 
-    public static void NodeInit(MachineNode myNode, MachineConfigurations machineConfig) {
+    public static void NodeInit(MachineNode myNode, MachineConfigurations machineConfig)
+    {
         myNode.setMachineID(machineConfig.getMachine_id());
         myNode.setNodeIp(machineConfig.getIp());
         MainHandler_Node.node = myNode;
