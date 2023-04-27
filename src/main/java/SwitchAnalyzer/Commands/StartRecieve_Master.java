6b63d13e7e4fcbe4e9d5639df755b9d8635c4a5b
Mostafa A. Tawfik <85037618+MostafaAhmed264@users.noreply.github.com>
2023-04-27 04:31:04 +0200

    Update StartRecieve_Master.java

--- src/main/java/SwitchAnalyzer/Commands/StartRecieve_Master.java
+++ src/main/java/SwitchAnalyzer/Commands/StartRecieve_Master.java
@@ -1,20 +1,17 @@
 package SwitchAnalyzer.Commands;
 
-import SwitchAnalyzer.Database.DBConnect;
-import SwitchAnalyzer.Database.DBInsert;
 import SwitchAnalyzer.Kafka.Topics;
 import SwitchAnalyzer.Machines.MachineNode;
 import SwitchAnalyzer.MainHandler_Master;
 import SwitchAnalyzer.Network.ErrorDetection.ErrorDetectingAlgorithms;
 import SwitchAnalyzer.Network.FrameProcessing;
-import SwitchAnalyzer.Network.PacketInfo;
 import SwitchAnalyzer.miscellaneous.JSONConverter;
 
 import static SwitchAnalyzer.MainHandler_Master.master;
 
 public class StartRecieve_Master  extends ICommandMaster
 {
-    public static ErrorDetectingAlgorithms errorDetectingAlgorithm;
+    public ErrorDetectingAlgorithms errorDetectingAlgorithm;
     StartRecieve_Master(){ errorDetectingAlgorithm = null; this.portID = 0; }
     public void processCmd()
     {
@@ -24,7 +21,7 @@ public class StartRecieve_Master  extends ICommandMaster
     private void openProcessThread()
     {
         Thread t = new Thread (() -> {
-            FrameProcessing.errorDetectingAlgorithms = this.errorDetectingAlgorithm;
+            FrameProcessing.errorDetectingAlgorithms = errorDetectingAlgorithm;
             FrameProcessing.startProcessFrames();
         });
         t.start();
