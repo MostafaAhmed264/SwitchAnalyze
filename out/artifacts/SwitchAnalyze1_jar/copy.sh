46cf32a5ab2a313159f68888598a3f98233cf50e
ZiadFahmy <zyadfahmy2@gmail.com>
2023-06-14 01:12:23 +0300

    Create copy.sh

---
+++ out/artifacts/SwitchAnalyze1_jar/copy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+scp SwitchAnalyze1.jar switchanalyze@192.168.1.111:/home/switchanalyze/SwitchAnalyze.jar #adel
+scp SwitchAnalyze1.jar switchanalyze@192.168.1.100:/home/switchanalyze/SwitchAnalyze.jar
+scp SwitchAnalyze1.jar switchanalyze@192.168.1.60:/home/switchanalyze/SwitchAnalyze.jar
+scp SwitchAnalyze1.jar switchanalyze@192.168.1.35:/home/switchanalyze/SwitchAnalyze.jar
+scp SwitchAnalyze1.jar switchanalyze@192.168.1.3:/home/switchanalyze/SwitchAnalyze.jar
